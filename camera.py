b1173783e694fd834ffe2ef6ef5f9a14afdcd294
Leland <lelandmball@gmail.com>
2018-11-04 00:27:40 -0400

    Code review by Leland

--- camera.py
+++ camera.py
@@ -33,7 +33,7 @@ def find_pom(color):
     while True:
         camera_update()
         if get_object_count(color) > 0:
-            camera_update() # This needs to only be run once per loop. It is unnecessary here.
+            camera_update() # This needs to only be run once per loop. It is unnecessary here. Remove this one <-- LMB
             if camera_center == get_object_center_x(color, 0):
                 print("I see something")
                 print(get_object_center_x(color, 0))
@@ -54,11 +54,10 @@ def find_pom(color):
 
 
 
-				# May I submit to you that this line^ never runs... (probably)...
-				# because you've already checked (via get_object_count()) that you are indeed seeing SOMETHING. 
-				# You need to add another case to your get_object_count() check. What will your robot do if it really
-				# doesn't see any objects of the color you're looking for? - LMB
-
+# I like the two more "gradual" cases you programmed.
+# You probably knew this already, but I like how the test for 40 < x < 70 will run first and
+# exit the if statement, while the next check for 90 < x < 120 would've also been "triggered",
+# but since you have the more specific/smaller width first, your robot executes that code instead -LMB
 def find_pom_improved(color): #Needs testing
     print("Searching for poms")
     camera_update_wrks()
@@ -66,7 +65,7 @@ def find_pom_improved(color): #Needs testing
     while True:
         camera_update()
         if get_object_count(color) > 0:
-            camera_update() # Only  needs to be called once per loop
+            camera_update() # Only  needs to be called once per loop. Remove this one <-- LMB
             if get_object_center_x(color, 0) < 40:
                 motor(c.LEFT_MOTOR, 0)
                 motor(c.RIGHT_MOTOR, 40)
@@ -84,8 +83,7 @@ def find_pom_improved(color): #Needs testing
                 motor(c.RIGHT_MOTOR, 40)
         else:
             m.drive_timed(-10, 10, 1)
-		# Again, you probably need another case here. How does your robot search for more poms?
-		# see above comments on your similar function
+
 
 
 def find_pom_best(color): #Needs testing
@@ -161,6 +159,8 @@ def get_can(color): #Does not work yet :(
         elif get_object_count(c.CHANNEL_RED) > 0 and area_average >= 1600:
             m.drive_timed(25, 25, 1000)
             print(area_average)
-            break
+            break	# (Is this for debugging?) dunno if you want this. "break" will hop out of your while loop 
+					# If you still find that you need logic like this, then there's probably a 
+					# way to re-write the While loop -LMB
         # else:
         #     m.drive_timed(-10, 10, 1)
